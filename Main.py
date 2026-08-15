d3aa9402af5f104bc79e842c11bb736aece4d6a2
dhavaldesai530-blip <dhavaldesai530@gmail.com>
2026-08-15 16:17:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Telegram Credentials
 TELEGRAM_BOT_TOKEN = "8868194866:AAHUmnjNV_dKKvzPtI_RrxnOZ3AeflB7kxM
-TELEGRAM_CHAT_ID = "vcpkrishnabot"
+TELEGRAM_CHAT_ID = "774112167"
 
 def send_telegram_alert(message):
     try:
